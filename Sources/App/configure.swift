4ce677f5610aa3ab08b5bc2ffc8324f2ada727ba
Maarten Engels <maartene@mac.com>
2019-03-22 13:18:11 +0100

    move from SQLite to PostgreSQL

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,4 +1,4 @@
-import FluentSQLite
+import FluentPostgreSQL
 import Vapor
 import Leaf
 import Authentication
@@ -6,7 +6,7 @@ import Authentication
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     // Register providers first
-    try services.register(FluentSQLiteProvider())
+    try services.register(FluentPostgreSQLProvider())
 
     // Register routes to the router
     let router = EngineRouter.default()
@@ -20,16 +20,16 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     services.register(middlewares)
 
     // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .file(path: "db.sqlite"))
+    let postgresql = try PostgreSQLDatabase(config: PostgreSQLDatabaseConfig(hostname: "127.0.0.1", port: 5432, username: "maartene", database: "todos", password: nil, transport: .cleartext))
 
     // Register the configured SQLite database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    databases.add(database: postgresql, as: .psql)
     services.register(databases)
 
     // Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: Todo.self, database: .psql)
     services.register(migrations)
     
     // Configure Leaf
